092a63d6009ac6e55898d37d10d70abb46643163
tx-spp <yamashita.hideyuki@po.ntt-tx.co.jp>
2020-02-20 14:08:41 +0900

    Initial commit

---
+++ bin/spp_pri.sh
@@ -0,0 +1,99 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(c) 2019 Nippon Telegraph and Telephone Corporation
+
+# Activate for debugging
+#set -x
+
+SPP_PRI_VHOST=""
+SPP_PRI_RING=""
+SPP_PRI_TAP=""
+SPP_PRI_MEMIF=""
+SPP_PRI_VDEVS=""
+
+SOCK_VHOST="/tmp/sock"
+SOCK_MEMIF="/tmp/spp-memif.sock"
+
+function clean_sock_files() {
+    # clean /tmp/sock*
+    sudo rm -f ${SOCK_VHOST}*
+    sudo rm -f ${SOCK_MEMIF}
+}
+
+# Add vhost vdevs named as such as `eth_vhost0`.
+function setup_vhost_vdevs() {
+    if [ ${PRI_VHOST_VDEVS} ]; then
+        for id in ${PRI_VHOST_VDEVS[@]}; do
+            SPP_PRI_VHOST="${SPP_PRI_VHOST} --vdev eth_vhost${id},iface=${SOCK_VHOST}${id}"
+        done
+    fi
+}
+
+# Add ring vdevs named as such as `net_ring0`.
+function setup_ring_vdevs() {
+    if [ ${PRI_RING_VDEVS} ]; then
+        for id in ${PRI_RING_VDEVS[@]}; do
+            SPP_PRI_RING="${SPP_PRI_RING} --vdev net_ring${id}"
+        done
+    fi
+}
+
+# Add tap vdevs named as such as `net_tap0`.
+function setup_tap_vdevs() {
+    if [ ${PRI_TAP_VDEVS} ]; then
+        for id in ${PRI_TAP_VDEVS[@]}; do
+            SPP_PRI_TAP="${SPP_PRI_TAP} --vdev net_tap${id},iface=vtap${id}"
+        done
+    fi
+}
+
+# Add memif vdevs named as such as `net_memif`.
+function setup_memif_vdevs() {
+    if [ ${PRI_MEMIF_VDEVS} ]; then
+        for id in ${PRI_MEMIF_VDEVS[@]}; do
+            SPP_PRI_MEMIF="${SPP_PRI_MEMIF} --vdev net_memif${id},id=${id},role=master,socket=${SOCK_MEMIF}"
+        done
+    fi
+}
+
+# Add any of vdevs.
+function setup_vdevs() {
+    if [ ${PRI_VDEVS} ]; then
+        for vdev in ${PRI_VDEVS[@]}; do
+            SPP_PRI_VDEVS="${SPP_PRI_VDEVS} --vdev ${vdev}"
+        done
+    fi
+}
+
+# Launch spp_primary.
+function spp_pri() {
+    SPP_PRI_BIN=${SPP_DIR}/src/primary/${RTE_TARGET}/spp_primary
+
+    if [ ${SPP_FILE_PREFIX} ]; then
+        FILE_PREFIX_OPT="--file-prefix ${SPP_FILE_PREFIX}"
+    fi
+
+    cmd="sudo ${SPP_PRI_BIN} \
+        -l ${PRI_CORE_LIST} \
+        -n ${PRI_MEMCHAN} \
+        --socket-mem ${PRI_MEM} \
+        --huge-dir ${SPP_HUGEPAGES} \
+        --proc-type primary \
+        --base-virtaddr 0x100000000 \
+        ${FILE_PREFIX_OPT} \
+        --log-level ${LOGLEVEL} \
+        ${SPP_PRI_VHOST} \
+        ${SPP_PRI_RING} \
+        ${SPP_PRI_TAP} \
+        ${SPP_PRI_MEMIF} \
+        ${SPP_PRI_VDEVS} \
+        -- \
+        -p ${PRI_PORTMASK} \
+        -n ${NUM_RINGS} \
+        -s ${SPP_CTL_IP}:5555"
+
+    if [ ${DRY_RUN} ]; then
+        echo ${cmd}
+    else
+        ${cmd} > ${SPP_DIR}/log/${PRI_LOG} 2>&1 &
+    fi
+}
